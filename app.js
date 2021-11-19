9da445c6d669894bee022c2c669c3f2fdfc23f85
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-11-19 18:58:24 +0300

    feat: cors, logger, central errors

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate'); // для обработки ошибок joi, celebrate
+const cors = require('cors'); // пакет node.js
 const router = require('./routes/index');
 const auth = require('./middlewares/auth');
+const centralizedErrors = require('./middlewares/centralizedErrors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3001, MONGO_URL = 'mongodb://localhost:27017/moviesdb' } = process.env;
 
@@ -11,10 +15,28 @@ const app = express();
 
 mongoose.connect(MONGO_URL);
 
+const options = { // TODO убрать в utils?
+  origin: [
+    'http://localhost:3000',
+    // 'http://mesto.tmweb.ru',
+    // 'https://mesto.tmweb.ru',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
+app.use('*', cors(options)); // ПЕРВЫМ!
 app.use(cookieParser());
 app.use(express.json());
+app.use(requestLogger); // подключаем логгер запросов
 app.use(auth);
 app.use(router);
+app.use(errorLogger); // подключаем логгер ошибок
+app.use(errors()); // обработчик ошибок celebrate
+app.use(centralizedErrors); // централизованная обработка ошибок
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
